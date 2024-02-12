dd587401598a8340855e247a2978b48cfa3eb61a
CyberGui23 <158279942+CyberGui23@users.noreply.github.com>
2024-02-12 12:36:50 +0200

    Add files via upload

---
+++ page4.js
@@ -0,0 +1,184 @@
+
+const randomTimeSquare = document.getElementById("randomTime");
+const generateInfoBtn = document.getElementById("generateInfoBtn");
+const randomInfoDisplay = document.getElementById("randomInfo");
+const backBtn = document.getElementById("backBtn");
+const iframe = document.querySelector("iframe");
+
+// Atualiza o URL do site dentro do iframe quando a navegação ocorre
+iframe.addEventListener("load", () => {
+  document.getElementById("siteUrl").textContent = iframe.contentWindow.location.href;
+});
+
+backBtn.addEventListener("click", () => {
+  window.history.back();
+});
+// Função para gerar informações aleatórias
+function generateRandomInfo() {
+  const randomInfoArray = [
+    'Rosa...🚀', 'Rosa...🚀', '2X🔁', '7X', '9X⚡', '2X','3X ', '2X', '2X','3X ', '2X','3X ', '4X', '5X⭐️', '4X', '5X⭐️', '4X', '5X⭐️', 'Rosa...🚀',
+    '3X ', '2X', '8X', '6X', '4X', '5X⭐️', '2X','3X ', '2X', '2X','3X ', '2X','3X ', '2X','3X ', '2X','3X ', '2X','3X ', 'Rosa...🚀', 'Rosa...🚀',
+  ];
+  const randomIndex = Math.floor(Math.random() * randomInfoArray.length);
+  return randomInfoArray[randomIndex];
+}
+
+// Atualiza o horário atual
+function updateCurrentTime() {
+  const currentTime = new Date().toLocaleTimeString();
+  document.getElementById("currentTime").textContent = currentTime;
+}
+const randomTimeDisplay = document.getElementById("randomTime");
+randomTimeDisplay.textContent = randomTime;
+
+
+// Atualiza informações aleatórias
+function updateRandomInfo() {
+  const randomInfo = generateRandomInfo();
+  document.getElementById("randomInfo").textContent = randomInfo;
+}
+
+// Função para gerar um horário aleatório com um intervalo de 2 minutos
+function generateRandomTime() {
+  const currentTime = new Date();
+  const randomSeconds = Math.floor(Math.random() * 30); // Número entre 0 e 29
+  currentTime.setSeconds(currentTime.getSeconds() + randomSeconds);
+  return `${currentTime.toLocaleTimeString()}`;
+}
+
+// Atualiza o horário aleatório e as informações a cada 2 minutos
+function updateRandomTimeAndInfo() {
+  const randomTime = generateRandomTime();
+  const randomInfo = generateRandomInfo();
+
+  document.getElementById("randomTime").textContent = randomTime;
+  document.getElementById("randomInfo").textContent = randomInfo;
+}
+
+// Atualiza o tempo a cada segundo
+setInterval(updateCurrentTime, 1000);
+
+// Atualiza as informações aleatórias a cada 5 segundos
+setInterval(updateRandomInfo, 60000);
+
+// Atualiza o horário aleatório e as informações a cada 2 minutos
+setInterval(updateRandomTimeAndInfo, 60000); // 2 minutos em milissegundos
+
+const overlay = document.getElementById("overlay");
+const verifyButton = document.getElementById("verifyButton");
+
+verifyButton.addEventListener("click", () => {
+  const verificationCode = document.getElementById("verificationCode").value;
+  
+  // Verificar se o código inserido é válido (coloque sua lógica de verificação aqui)
+  if (verificationCode === "####") {
+    // Código de verificação correto, remova a sobreposição
+    overlay.style.display = "none";
+  } else {
+    // Código de verificação incorreto, você pode exibir uma mensagem de erro aqui
+    alert("Voucher incorreto. Tente novamente.");
+  }
+});
+// ...
+function updateProgressBar() {
+  const progressBar = document.getElementById("progressBar");
+  const percentageLabel = document.getElementById("percentageLabel");
+
+  // Gere uma porcentagem aleatória entre 30 e 80
+  const randomPercentage = Math.floor(Math.random() * (100 - 30 + 1)) + 30;
+
+  // Atualize a largura da barra de progresso
+  progressBar.style.width = `${randomPercentage}%`;
+
+  // Atualize o rótulo de porcentagem
+  percentageLabel.textContent = `${randomPercentage}%`;
+
+  // Agende a próxima atualização após 30 minutos (1800000 milissegundos)
+  setTimeout(updateProgressBar, 60000);
+}
+
+// Inicialize a atualização da barra de progresso
+updateProgressBar();
+
+function updateMarketPercentage() {
+  const marketPercentageBar = document.getElementById("marketPercentageFill");
+  const marketPercentageLabel = document.getElementById("marketPercentageLabel");
+
+  // Gere uma porcentagem aleatória entre 20 e 80
+  const randomPercentage = Math.floor(Math.random() * (80 - 20 + 1)) + 20;
+
+  // Atualize a largura da barra de porcentagem
+  marketPercentageBar.style.width = `${randomPercentage}%`;
+
+  // Atualize o rótulo de porcentagem
+  marketPercentageLabel.textContent = `${randomPercentage}%`;
+
+  // Agende a próxima atualização após 10 minutos (600000 milissegundos)
+  setTimeout(updateMarketPercentage, 600000);
+}
+
+// Inicialize a atualização da porcentagem do mercado
+updateMarketPercentage();
+
+// Adicione isso ao seu script JavaScript existente
+const infoBtn = document.getElementById("infoBtn");
+const modalOverlay = document.getElementById("modalOverlay");
+const closeModalBtn = document.getElementById("closeModalBtn");
+
+infoBtn.addEventListener("click", () => {
+  modalOverlay.style.display = "block";
+});
+
+closeModalBtn.addEventListener("click", () => {
+  modalOverlay.style.display = "none";
+});
+
+// Adicione isso ao seu script JavaScript existente
+const infoContent = document.getElementById("infoContent");
+
+function updateInfoContent() {
+  const currentMinute = new Date().getMinutes();
+
+  if (currentMinute >= 0 && currentMinute < 2) {
+    infoContent.textContent = "Possível 100X";
+  } else if (currentMinute >= 2 && currentMinute < 5) {
+    infoContent.textContent = "Possível 100X";
+  } else if (currentMinute >= 5 && currentMinute < 7) {
+    infoContent.textContent = "Possível 10/20/100X";
+  } else if (currentMinute >= 5 && currentMinute <= 6) {
+    infoContent.textContent = "Possível 10/20/100X";
+  } else if (currentMinute >= 16 && currentMinute <= 17) {
+    infoContent.textContent = "Possível 20X";
+  } else if (currentMinute >= 21 && currentMinute <= 22) {
+    infoContent.textContent = "Possível 10/30X";
+  } else if (currentMinute >= 26 && currentMinute <= 27) {
+    infoContent.textContent = "Possível 10/30X";
+  } else if (currentMinute >= 28 && currentMinute <= 29) {
+    infoContent.textContent = "Possível 10/20/100X";
+  } else if (currentMinute >= 31 && currentMinute <= 32) {
+    infoContent.textContent = "Possível 10/30X";
+  } else if (currentMinute >= 36 && currentMinute <= 37) {
+    infoContent.textContent = "Possível 10/20/50X";
+  } else if (currentMinute >= 41 && currentMinute <= 42) {
+    infoContent.textContent = "Possível 20/100X";
+  } else if (currentMinute >= 45 && currentMinute <= 46) {
+    infoContent.textContent = "Possível 10/100X";
+  } else if (currentMinute >= 48 && currentMinute <= 49) {
+    infoContent.textContent = "Possível 18/50X";
+  } else if (currentMinute >= 50 && currentMinute <= 51) {
+    infoContent.textContent = "Possível 10/18X";
+  } else if (currentMinute >= 53 && currentMinute <= 54) {
+    infoContent.textContent = "Possível 18X";
+  } else if (currentMinute >= 56 && currentMinute <= 57) {
+    infoContent.textContent = "Possível 28/100X";
+  } else {
+    // Outra lógica se necessário para outros intervalos
+    infoContent.textContent = "Analizando...";
+  }
+}
+
+// Atualiza a informação inicial
+updateInfoContent();
+
+// Atualiza a informação a cada minuto (60 segundos)
+setInterval(updateInfoContent, 60000);
---
+++ script.js
@@ -0,0 +1,98 @@
+
+  const accounts = [
+    { username: "CyberGui", password: "230801" },
+    { username: "Maricoua", password: "4321" },
+    // Adicione mais contas conforme necessário
+  ];
+  
+  // ... (mantenha o código anterior)
+  
+  const userContent = document.getElementById("userContent");
+  const loggedInUsername = document.getElementById("loggedInUsername");
+  const logoutBtn = document.getElementById("logoutBtn");
+  
+  loginBtn.addEventListener("click", () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    const account = accounts.find(acc => acc.username === username && acc.password === password);
+  
+    if (account) {
+      loggedInUsername.textContent = username;
+      showUserScreen();
+    } else {
+      Swal.fire("Credenciais inválidas. Tente novamente.", "", "error");
+    }
+  });
+  notificationBtn.addEventListener("click", () => {
+    Swal.fire({
+      title: "Bem Vindo ao NALDO BOT V1 Atenção: Crie sua conta enquanto estiver no bot.Para os usuarios com contas, aconselhamos a criar novas contas para aceder a versao com 98,9% de acertos. SUPORTE:842900642.",
+      icon: "info",
+      confirmButtonText: "Fechar"
+    });
+  });
+  
+  logoutBtn.addEventListener("click", () => {
+    showLoginScreen();
+  });
+  
+  function showUserScreen() {
+    userContent.style.display = "block";
+    document.querySelector(".login-container").style.display = "none";
+  }
+  
+  function showLoginScreen() {
+    userContent.style.display = "none";
+    document.querySelector(".login-container").style.display = "block";
+  }
+  
+  
+  // ... (mantenha o código anterior)
+  
+  const androidBtn = document.getElementById("androidBtn");
+  
+  document.querySelectorAll(".square").forEach((square, index) => {
+    square.addEventListener("click", () => {
+      redirectToNewPage(index + 1);
+    });
+  });
+  
+  androidBtn.addEventListener("click", () => {
+    window.open("https://chat.openai.com/", "_blank");
+  });
+  
+  function redirectToNewPage(pageNumber) {
+    switch (pageNumber) {
+      case 1:
+        // Redirecionar para a página 1
+        window.location.href = "page1.html";
+        break;
+      case 2:
+        // Redirecionar para a página 2
+        window.location.href = "page2.html";
+        break;
+      case 3:
+        // Redirecionar para a página 3
+        window.location.href = "page3.html";
+        break;
+      case 4:
+        // Redirecionar para a página 4
+        window.location.href = "page4.html";
+        break;
+      case 5:
+        // Redirecionar para a página 5
+        window.location.href = "page5.html";
+        break;
+      case 6:
+        // Redirecionar para a página 6
+        window.location.href = "page6.html";
+        break;
+      case 7:
+        // Redirecionar para a página 7
+        window.location.href = "page7.html";
+        break;
+      default:
+        break;
+    }
+  }
+  
