b36345ffb71dfb92b26b64ae26158c70a1b31efe
CyberGui23 <158279942+CyberGui23@users.noreply.github.com>
2024-02-26 16:06:25 +0200

    script.js
    Gshshs

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
   const accounts = [
     { username: "CyberGui", password: "******" },
     { username: "Maricoua", password: "$$$$" },
-    { username: "343Mafupa", password: "001122" },
     { username: "13Kelton", password: "334455" },
     // Adicione mais contas conforme necessário
   ];
